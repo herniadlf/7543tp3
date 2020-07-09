437390050d35d2c95e54d73b21f7bf264e50e1e4
Mariano Hielpos <mariano.hielpos@despegar.com>
2020-07-09 17:31:47 -0300

    Fix link

--- controller/extensions/link.py
+++ controller/extensions/link.py
@@ -1,15 +1,16 @@
-class Link:
+LINK_ATTRIBUTES=["dpid1", "port1", "dpid2", "port2"]
+
+class Link(object):
     def __init__(self, link):
         self.link = link
         self.weight = 0
 
-    """ def __getattr__(self, key):
-        if key == 'weight':
-            return self.weight
-        return self.link.__getattribute__(key)
+    def __getattr__(self, key):
+        if key in LINK_ATTRIBUTES:
+            return self.link.__getattr__(key)
+        return super(Link, self).__getattr__(key)
 
     def __setattr__(self, key, value):
-        if key == 'weight':
-            self.weight = value
-        else:
-            self.link.__setattribute__(key, value) """
+        if key in LINK_ATTRIBUTES:
+            return self.link.__setattr__(key, value)
+        return super(Link, self).__setattr__(key, value)
